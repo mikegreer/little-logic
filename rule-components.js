74692656f8298e42b5ee3825f79f236c77479068
mikegreer <mr.greer@gmail.com>
2017-08-05 11:19:37 +0100

    starting point for rule editing tooltips

--- rule-components.js
+++ rule-components.js
@@ -14,6 +14,12 @@ drop.addEventListener('click', function(e){
     drop.setAttribute('dcolor', nextDrop);
     drop.style.fill = dropColors[nextDrop];
 });
+drop.addEventListener('mouseenter', function(e){
+    showTooltip(e.target);
+});
+drop.addEventListener('mouseleave', function(e){
+    hideTooltip();
+});
 
 //direction picker
 arrow.setAttribute('direction', 0);
@@ -22,7 +28,13 @@ arrow.addEventListener('click', function(e){
     var nextDirection = currentDirection + directionIncrements;
     arrow.setAttribute('direction', nextDirection);
     arrow.style.transform = "rotate("+nextDirection+"deg)";
-})
+});
+arrow.addEventListener('mouseenter', function(e){
+    showTooltip(e.target);
+});
+arrow.addEventListener('mouseleave', function(e){
+    hideTooltip();
+});
 
 //shape selector
 Math.radians = function(degrees) {
@@ -68,6 +80,12 @@ shape.addEventListener('click', function(e){
     shape.setAttribute('points', nextPoints);
     tweenPoints(shape, currentPoints, nextPoints);
 });
+shape.addEventListener('mouseenter', function(e){
+    showTooltip(e.target);
+});
+shape.addEventListener('mouseleave', function(e){
+    hideTooltip();
+});
 
 function tweenPoints(shape, from, to){
     shape.style.transition = "all 300ms ease-in"
@@ -81,9 +99,59 @@ function tweenPoints(shape, from, to){
             if(from >= 3){
                 shape.setAttribute('d', generatePath(from));
             }else{
-                interval = null;
-                console.log(interval);
+                clearTimeout(interval);
             }
         }, 150);
     }
+}
+
+//music selector
+var drum = document.querySelector('.drum');
+var marker = document.querySelector('.drum-marker');
+var nextSample = 0;
+drum.addEventListener('click', function(e){
+    nextSample++;
+    var currentX = nextSample % 3;
+    var currentY = Math.floor(nextSample/3) % 3;
+    marker.setAttribute('x', currentX * 14.5 + 4);
+    marker.setAttribute('y', currentY * 14.5 + 3.5);
+});
+drum.addEventListener('mouseenter', function(e){
+    showTooltip(e.target);
+});
+drum.addEventListener('mouseleave', function(e){
+    hideTooltip();
+});
+
+//tootltip
+var tooltip = document.querySelector('.tooltip');
+var tooltipBody = document.querySelector('.tooltip-body');
+var tooltipTail = document.querySelector('.tail');
+function showTooltip(target){
+    //xpos = middle of target - half width of tooltip
+    //      = bounds.left + ((bounds.right - bounds.left) / 2) - half width of tooltip
+    var tipBounds = tooltip.getBoundingClientRect();
+    var bounds = target.getBoundingClientRect();
+    xpos = bounds.left + (bounds.width / 2) - (tipBounds.width / 2);
+    ypos = bounds.top - 80;
+    tooltip.style.left = xpos+"px";
+    tooltip.style.top = ypos+"px";
+    tooltipBody.style.height = "20px";
+    tooltipBody.style.padding = "16px 30px";
+    window.setTimeout(function(){
+        tooltipTail.style.display = "block";
+        window.setTimeout(function(){
+            tooltipTail.style.bottom = "9px";
+        }, 100);
+    }, 200);
+}
+function hideTooltip(){
+    tooltipTail.style.bottom = "19px";
+    window.setTimeout(function(){
+        tooltipTail.style.display = "none";
+        window.setTimeout(function(){
+            tooltipBody.style.height = "0";
+            tooltipBody.style.padding = "0 30px";
+        }, 50);
+    }, 100);
 }
