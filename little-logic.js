d89db923c21ca200fcb4b0e5455b2f2c92dbd1da
Mike Greer <mr.greer@gmail.com>
2018-05-01 10:53:44 +0100

    trails objects

--- little-logic.js
+++ little-logic.js
@@ -1,37 +1,49 @@
 var ruleEditor = document.querySelector('rule-editor');
 
 //set default rule
-var rule = {
-    'ruleId': 0,
-    'dropcolor': 0,
-    'points': 3,
-    'direction': 0,
-    'sample': 0
-}
-ruleEditor.setAttribute('rule', JSON.stringify(rule));
-
-document.querySelector('#rule1').addEventListener('click', function(e) {
-    ruleEditor.setAttribute('rule', JSON.stringify(
-        {
+var rules = [
+    {
+        'ruleId': 0,
+        'dropcolor': 0,
+        'points': 3,
+        'direction': 0,
+        'sample': 0
+    },
+    {
         'ruleId': 1,
         'dropcolor': 4,
         'points': 3,
         'direction': 180,
         'sample': 8
-    }));
-});
-
-document.querySelector('#rule2').addEventListener('click', function(e) {
-    ruleEditor.setAttribute('rule', JSON.stringify(
-        {
+    },
+    {
         'ruleId': 2,
         'dropcolor': 5,
         'points': 5,
         'direction': 225,
         'sample': 4
-    }));
+    }
+];
+
+currentRule = 0;
+function setRule(ruleID){
+    currentRule = ruleID;
+    ruleEditor.setAttribute('rule', JSON.stringify(rules[ruleID]));
+}
+setRule(0);
+
+document.querySelector('#rule1').addEventListener('click', function(e) {
+    setRule(1);
+});
+
+document.querySelector('#rule2').addEventListener('click', function(e) {
+    setRule(2);
 });
 
 ruleEditor.addEventListener('ruleChange', function(e){
-    console.log(e.detail.value);
+    console.log(currentRule, e.detail.ruleId);
+    if(e.detail.ruleId === currentRule){
+        console.log('replace');
+        rules[currentRule] = e.detail;
+    }
 });
--- trails.js
+++ trails.js
@@ -3,13 +3,6 @@ canvas.width = 800;
 canvas.height = 400;
 var ctx = canvas.getContext('2d');
 
-var radius = 70;
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
-var divisions = 13;
-var divisionHeight = radius*2 / divisions;
-var trailLines = [];
-
 var bgColor = '#000F6D';
 var shapeColor = '#00FF6D';
 var shadowColor = '#0AE864';
@@ -21,58 +14,98 @@ function drawCircle(x, y, r, color) {
     ctx.fill();
 }
 
-function setup(){
-    for(var i = 0; i < divisions; i++){
-        var divisionY = centerY - radius + divisionHeight*i + divisionHeight/2;
-        var distToCurve = Math.sqrt((radius * radius) - (centerY - divisionY) * (centerY - divisionY));
-        var baseLength = distToCurve + radius;
-        trailLines.push({
+//example shape object
+// var shape = {
+//     trailLines : [{
+//         'y' : divisionY,
+//         'baseLength' : baseLength + baseLength * Math.random(),
+//         'shadowLength' : Math.random() * radius/6 + radius / 4
+//     },{
+//         'y' : divisionY,
+//         'baseLength' : baseLength + baseLength * Math.random(),
+//         'shadowLength' : Math.random() * radius/6 + radius / 4
+//     }],
+//     x : 200,
+//     y : 200,
+//     radius : 70,
+//     blurSegments : 17,
+//     primaryColor : '#00FF6D',
+//     secondaryColor : '#0AE864'
+// }
+
+var shapes = [];
+
+function createShape(shapeX, shapeY, shapeR, blurSegments, shapeColor, shadowColor){
+    var shape = {
+        trailLines : [],
+        x : shapeX,
+        y : shapeY,
+        radius : shapeR,
+        blurSegments : blurSegments,
+        primaryColor : shapeColor,
+        secondaryColor : shadowColor
+    };
+    var segmentHeight = shapeR * 2 / blurSegments;
+    shape.trailLines = [];
+    for(var i = 0; i < blurSegments; i++){
+        var divisionY = shapeY - shapeR + segmentHeight*i + segmentHeight/2;
+        var distToCurve = Math.sqrt((shapeR * shapeR) - (shapeY - divisionY) * (shapeY - divisionY));
+        var baseLength = distToCurve + shapeR;
+        shape.trailLines.push({
             'y' : divisionY,
             'baseLength' : baseLength + baseLength * Math.random(),
-            'shadowLength' : Math.random() * radius/6 + radius / 4
+            'shadowLength' : Math.random() * shapeR/6 + shapeR / 4
         });
     }
-    for(var j = 0; j < divisions; j++){
-        var line = trailLines[j];
+    for(var j = 0; j < blurSegments; j++){
+        var line = shape.trailLines[j];
         if(j%2){
-            if(line.baseLength > trailLines[j-1].baseLength){
-                line.baseLength = trailLines[j-1].baseLength / 2;
+            if(line.baseLength > shape.trailLines[j-1].baseLength){
+                line.baseLength = shape.trailLines[j-1].baseLength / 2;
             }
-            if(j != divisions - 1){
-                if(line.baseLength > trailLines[j+1].baseLength){
-                    line.baseLength = trailLines[j+1].baseLength / 2;
+            if(j != blurSegments - 1){
+                if(line.baseLength > shape.trailLines[j+1].baseLength){
+                    line.baseLength = shape.trailLines[j+1].baseLength / 2;
                 }
             }
         }
     }
+    return shape;
 }
-setup();
+shapes.push(createShape(220, 300, 30, 9, '#FFDC00', '#FFBA0A'));
+shapes.push(createShape(100, 100, 20, 5, '#FFDC00', '#FFBA0A'));
+shapes.push(createShape(canvas.width / 2, canvas.height / 2, 70, 17, '#00FF6D', '#0AE864'));
 
 function draw(){
+    //bg
     ctx.fillStyle = bgColor;
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fill();
-    drawCircle(centerX, centerY, radius, shapeColor);
 
-    for(var i = 0; i < divisions; i++){
-        // var velocity = 5;
-        var line = trailLines[i];
-        ctx.beginPath();
-        ctx.strokeStyle = shapeColor;
-        ctx.lineWidth = divisionHeight + 1;
-        ctx.lineCap = "round";
-        lineLength = line.baseLength;
-        ctx.moveTo(centerX, line.y);
-        ctx.lineTo(centerX + lineLength, line.y);
-        ctx.stroke();
-        if(i%2){
+    //shapes
+    for(var i = 0; i < shapes.length; i++){
+        var shape = shapes[i];
+        drawCircle(shape.x, shape.y, shape.radius, shape.primaryColor);
+        for(var j = 0; j < shape.blurSegments; j++){
+            // var velocity = 5;
+            var divisionHeight = shape.radius * 2 / shape.blurSegments;
+            var line = shape.trailLines[j];
+            var lineLength = line.baseLength;
             ctx.beginPath();
-            ctx.strokeStyle = shadowColor;
-            lineLength = line.baseLength;
-            ctx.moveTo(centerX + lineLength, line.y);
-            ctx.lineTo(centerX + lineLength - line.shadowLength, line.y);
+            ctx.strokeStyle = shape.primaryColor;
+            ctx.lineWidth = divisionHeight + 1;
+            ctx.lineCap = "round";
+            ctx.moveTo(shape.x, line.y);
+            ctx.lineTo(shape.x + lineLength, line.y);
             ctx.stroke();
-            drawCircle(centerX + lineLength + 3, line.y, divisionHeight/2 - .5, bgColor)
+            if(j%2){
+                ctx.beginPath();
+                ctx.strokeStyle = shape.secondaryColor;
+                ctx.moveTo(shape.x + lineLength, line.y);
+                ctx.lineTo(shape.x + lineLength - line.shadowLength, line.y);
+                ctx.stroke();
+                drawCircle(shape.x + lineLength + 3, line.y, divisionHeight/2 - .5, bgColor)
+            }
         }
     }
 }
