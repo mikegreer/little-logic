916f1ab7c75de3bff29fab19ea98004edad7c189
Mike Greer <mr.greer@gmail.com>
2018-05-01 21:23:39 +0100

    starting animation

--- trails.js
+++ trails.js
@@ -30,12 +30,13 @@ function drawCircle(x, y, r, color) {
 //     radius : 70,
 //     blurSegments : 17,
 //     primaryColor : '#00FF6D',
-//     secondaryColor : '#0AE864'
+//     secondaryColor : '#0AE864',
+//     stretch : 10
 // }
 
 var shapes = [];
 
-function createShape(shapeX, shapeY, shapeR, blurSegments, shapeColor, shadowColor){
+function createShape(shapeX, shapeY, shapeR, blurSegments, shapeColor, shadowColor, stretch){
     var shape = {
         trailLines : [],
         x : shapeX,
@@ -43,7 +44,8 @@ function createShape(shapeX, shapeY, shapeR, blurSegments, shapeColor, shadowCol
         radius : shapeR,
         blurSegments : blurSegments,
         primaryColor : shapeColor,
-        secondaryColor : shadowColor
+        secondaryColor : shadowColor,
+        stretch : stretch
     };
     var segmentHeight = shapeR * 2 / blurSegments;
     shape.trailLines = [];
@@ -72,12 +74,12 @@ function createShape(shapeX, shapeY, shapeR, blurSegments, shapeColor, shadowCol
     }
     return shape;
 }
-shapes.push(createShape(220, 300, 30, 9, '#FFDC00', '#FFBA0A'));
-shapes.push(createShape(100, 100, 20, 5, '#FFDC00', '#FFBA0A'));
-shapes.push(createShape(canvas.width / 2, canvas.height / 2, 70, 17, '#00FF6D', '#0AE864'));
 
 function draw(){
     //bg
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
     ctx.fillStyle = bgColor;
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fill();
@@ -85,12 +87,16 @@ function draw(){
     //shapes
     for(var i = 0; i < shapes.length; i++){
         var shape = shapes[i];
+        // shape.velocity += 0.0002
+        // shape.x -= shape.velocity * 20;
+        // shape.velocity *= 1.05;
+        // console.log(shape.x);
         drawCircle(shape.x, shape.y, shape.radius, shape.primaryColor);
         for(var j = 0; j < shape.blurSegments; j++){
             // var velocity = 5;
             var divisionHeight = shape.radius * 2 / shape.blurSegments;
             var line = shape.trailLines[j];
-            var lineLength = line.baseLength;
+            var lineLength = (line.baseLength + shape.stretch) * .8;
             ctx.beginPath();
             ctx.strokeStyle = shape.primaryColor;
             ctx.lineWidth = divisionHeight + 1;
@@ -108,5 +114,16 @@ function draw(){
             }
         }
     }
+    requestAnimationFrame(draw);
 }
-draw();
+
+shapes.push(createShape(620, 300, 30, 9, '#FFDC00', '#FFBA0A', 20));
+shapes.push(createShape(400, 100, 20, 5, '#FFDC00', '#FFBA0A', 5));
+shapes.push(createShape(550, canvas.height / 2, 70, 17, '#00FF6D', '#0AE864', 60));
+// shapes[0].velocity = .005;
+// shapes[1].velocity = .01;
+// shapes[2].velocity = .001;
+shapes[0].step = 0;
+shapes[1].step = 0;
+shapes[2].step = 0;
+requestAnimationFrame(draw);
